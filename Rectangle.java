e96f2bd86fe8b897d20fb15792008d96164350fd
IrinaPugaeva <32075745+IrinaPugaeva@users.noreply.github.com>
2017-09-29 00:56:49 +0300

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,22 @@
+class Rect {
+    double length;
+    double width;
+
+    Rect(double l, double w) {
+        length = l;
+        width = w;
+    }
+    double area() {
+        return length*width;
+    }
+}
+public class Rectangle {
+    public static void main(String[] args) {
+        Rect myrectangle = new Rect(14,32);
+
+        double area;
+        area = myrectangle.area();
+
+        System.out.println("Площадь прямоугольника" + area);
+    }
+}
